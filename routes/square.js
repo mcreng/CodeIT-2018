8798ea79cc49db102c2d80f1cbdeae9afabb908d
mcreng <tsehonam@gmail.com>
2018-09-21 22:58:43 +0800

    Initial commit

--- routes/square.js
+++ routes/square.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import axios from 'axios'
+import axios from "axios";
 var router = Router();
 
-router.post('/', function (req, res, next) {
-    var input = req.body['input'];
-    res.send(JSON.stringify(parseInt(input) * parseInt(input)));
+router.post("/", function(req, res, next) {
+  var input = req.body["input"];
+  res.send(JSON.stringify(parseInt(input) * parseInt(input)));
 });
-
 
 export default router;
