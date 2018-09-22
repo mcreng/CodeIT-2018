10b43b7c230de9debc56a55ff61f8fd557eb9d38
mcreng <tsehonam@gmail.com>
2018-09-23 01:28:09 +0800

    Modified dino

--- routes/dino.js
+++ routes/dino.js
@@ -14,6 +14,7 @@ function* subsets(array, offset = 0) {
 }
 
 router.post("/", function(req, res, next) {
+  console.log(req.body);
   var n = req.body["number_of_types_of_food"];
   var A = req.body["calories_for_each_type_for_raphael"];
   var B = req.body["calories_for_each_type_for_leonardo"];
@@ -25,31 +26,23 @@ router.post("/", function(req, res, next) {
   var SB = [...subsets(B)]
     .map(v => v.reduce((a, b) => a + b, 0))
     .sort((a, b) => a - b);
-  console.log(SA, SB);
   var counter = 0;
+  var new_counter = 0;
   for (var i = 0; i < SB.length; i++) {
-    var j = 0;
-    for (; j < SA.length; j++) {
-      if (SA[j] - SB[i] > delta) {
-        console.log(SA.length - j);
-        counter += SA.length - j;
-        break;
-      }
-    }
+    var index = SA.findIndex(el => el - SB[i] > delta);
+    if (index !== -1) {
+      new_counter += SA.length - index;
+    } else break;
   }
   for (var i = SB.length - 1; i >= 0; i--) {
-    var j = SA.length - 1;
-    for (; j >= 0; j--) {
-      if (SB[i] - SA[j] > delta) {
-        console.log(j + 1);
-        counter += j + 1;
-        break;
-      }
-    }
+    var index = SA.reverse().findIndex(el => SB[i] - el > delta);
+    if (index !== -1) {
+      new_counter += SA.length - index;
+    } else break;
+    SA.reverse();
   }
-  console.log(counter);
 
-  res.send({ result: SA.length * SB.length - counter });
+  res.send({ result: SA.length * SB.length - new_counter });
 });
 
 export default router;
