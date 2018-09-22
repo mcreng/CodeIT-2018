cdad94c21e9001cd63b4fb487905c919f33c6d9d
mcreng <tsehonam@gmail.com>
2018-09-23 03:16:37 +0800

    Use dp for dino

--- routes/dino.js
+++ routes/dino.js
@@ -1,49 +1,43 @@
 import { Router } from "express";
 var router = Router();
 
-// Generate all array subsets:
-function* subsets(array, offset = 0) {
-  while (offset < array.length) {
-    let first = array[offset++];
-    for (let subset of subsets(array, offset)) {
-      subset.push(first);
-      yield subset;
+var data = {};
+var n = 0;
+var A = null;
+var B = null;
+var amax = 0;
+function loop() {
+  for (var k = -amax; k < amax; k++) {
+    data[[0, k]] = 0;
+  }
+  data[[0, 0]] = 1;
+  for (var i = 1; i <= n; i++) {
+    for (var k = -amax; k < amax; k++) {
+      var sum =
+        data[[i - 1, k - A[i - 1]]] +
+        data[[i - 1, k + B[i - 1]]] +
+        data[[i - 1, k - A[i - 1] + B[i - 1]]] +
+        data[[i - 1, k]];
+      data[[i, k]] = sum ? sum : 0;
     }
   }
-  yield [];
 }
 
 router.post("/", function(req, res, next) {
   console.log(req.body);
-  var n = req.body["number_of_types_of_food"];
-  if (n >= 13) res.send(false);
-  var A = req.body["calories_for_each_type_for_raphael"];
-  var B = req.body["calories_for_each_type_for_leonardo"];
+  n = req.body["number_of_types_of_food"];
+  A = req.body["calories_for_each_type_for_raphael"];
+  B = req.body["calories_for_each_type_for_leonardo"];
   var delta = req.body["maximum_difference_for_calories"];
-
-  var SA = [...subsets(A)]
-    .map(v => v.reduce((a, b) => a + b, 0))
-    .sort((a, b) => a - b);
-  var SB = [...subsets(B)]
-    .map(v => v.reduce((a, b) => a + b, 0))
-    .sort((a, b) => a - b);
+  amax = A.reduce((a, b) => a + b, 0) + B.reduce((a, b) => a + b, 0) + 1;
+  loop();
   var counter = 0;
-  var new_counter = 0;
-  for (var i = 0; i < SB.length; i++) {
-    var index = SA.findIndex(el => el - SB[i] > delta);
-    if (index !== -1) {
-      new_counter += SA.length - index;
-    } else break;
-  }
-  for (var i = SB.length - 1; i >= 0; i--) {
-    var index = SA.reverse().findIndex(el => SB[i] - el > delta);
-    if (index !== -1) {
-      new_counter += SA.length - index;
-    } else break;
-    SA.reverse();
+  console.log(data);
+  for (var i = -delta; i <= delta; i++) {
+    counter += data[[n, i]];
   }
 
-  res.send({ result: SA.length * SB.length - new_counter });
+  res.send({ result: counter });
 });
 
 export default router;
