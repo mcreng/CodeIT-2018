daa5767ddb4575ebdd5fbb38c27e65b25d68cb36
mcreng <tsehonam@gmail.com>
2018-09-22 16:26:53 +0800

    Fixed imagesGPS format

--- routes/imagesGPS.js
+++ routes/imagesGPS.js
@@ -19,18 +19,21 @@ router.post("/", async function(req, res, next) {
 
   for (var i = 0; i < input.length; i++) {
     var file = input[i]["path"];
-    await download(file, "./tmp.jpg", async () => new Promise((resolve,reject)=>{
-      var data = exif.parseSync("./tmp.jpg");
-      var gps = data["GPSInfo"];
-      var lat = gps["GPSLatitude"][0];
-      var lon = gps["GPSLongitude"][0];
-      output.push({ lat: lat, lon: lon });
-      console.log(output);
-    })
+    await download(
+      file,
+      "./tmp.jpg",
+      async () =>
+        new Promise((resolve, reject) => {
+          var data = exif.parseSync("./tmp.jpg");
+          var gps = data["GPSInfo"];
+          var lat = gps["GPSLatitude"][0];
+          var lon = gps["GPSLongitude"][0];
+          output.push({ lat: lat, long: lon });
+          console.log(output);
+        })
     );
   }
 
-  console.log(13);
   res.send(output);
 });
 
