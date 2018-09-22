abaa33c0aeeaf1c49d77c089d6c941bff1c22ba7
Kevin Crylz <33240066+kevinCrylz@users.noreply.github.com>
2018-09-22 17:37:56 +0800

    Add files via upload

---
+++ routes/customers-and-hotel/index.js
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import { PythonShell } from "python-shell";
+import axios from "axios";
+import { spawn } from "child_process";
+var router = Router();
+
+router.post("/minimum-camps", function(req, res, next) {
+  // console.log()
+  const formData = req.body
+
+  var options = {
+    mode: "text",
+    pythonPath: "python",
+    pythonOptions: ["-u"],
+    scriptPath: "./routes/customers-and-hotel/",
+    args: [formData]
+  }; // get print results in real-time
+
+  PythonShell.run("minimum-camps.py", options, function(err, results) {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    console.log("results: %j", results);
+    res.send({ answer: results });
+  });
+});
+router.post("/minimum-distance", function(req, res, next) {
+  // console.log()
+  const formData = req.body
+
+  var options = {
+    mode: "text",
+    pythonPath: "python",
+    pythonOptions: ["-u"],
+    scriptPath: "./routes/customers-and-hotel/",
+    args: [formData]
+  }; // get print results in real-time
+
+  PythonShell.run("minimum-distance.py", options, function(err, results) {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    console.log("results: %j", results);
+    res.send({ answer: results });
+  });
+});
+
+module.exports = router
---
+++ routes/customers-and-hotel/minimum-camps.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.argv[1]
+
+#input = [4, 7, 5, 1, 12, 13, 9, 2, 6, 3, 21]
+
+input.sort()
+
+min = max(input)
+for i in range(0,len(input)-1):
+    if (input[i+1] - input[i]) < min: min = (input[i+1] - input[i])
+
+print(min)
+sys.stdout.flush()
