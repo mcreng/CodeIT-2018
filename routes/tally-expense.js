34f265cf253c18028a7d871b26e0c5d09a68e20e
Kevin Crylz <33240066+kevinCrylz@users.noreply.github.com>
2018-09-23 02:51:02 +0800

    Update tally-expense.js

--- routes/tally-expense.js
+++ routes/tally-expense.js
@@ -19,7 +19,7 @@ router.post("/", function(req, res, next) {
     if (err) throw err;
     // results is an array consisting of messages collected during execution
     console.log("results: %j", results);
-    res.send(JSON.parse(results));
+    res.send({ transactions: JSON.parse(results)});
     console.log(JSON.parse(results))
   });
 });
