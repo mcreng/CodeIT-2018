5ce6298510f163118d484a5efd9810eeabcce9de
mcreng <tsehonam@gmail.com>
2018-09-23 00:52:02 +0800

    dino partial score, dont know if correct

--- routes/dino.js
+++ routes/dino.js
@@ -19,19 +19,37 @@ router.post("/", function(req, res, next) {
   var B = req.body["calories_for_each_type_for_leonardo"];
   var delta = req.body["maximum_difference_for_calories"];
 
-  var PA = subsets(A);
+  var SA = [...subsets(A)]
+    .map(v => v.reduce((a, b) => a + b, 0))
+    .sort((a, b) => a - b);
+  var SB = [...subsets(B)]
+    .map(v => v.reduce((a, b) => a + b, 0))
+    .sort((a, b) => a - b);
+  console.log(SA, SB);
   var counter = 0;
-  for (var pa of PA) {
-    var PB = subsets(B);
-    for (var pb of PB) {
-      Math.abs(pa.reduce((a, b) => a + b, 0) - pb.reduce((a, b) => a + b, 0)) <=
-      delta
-        ? counter++
-        : null;
+  for (var i = 0; i < SB.length; i++) {
+    var j = 0;
+    for (; j < SA.length; j++) {
+      if (SA[j] - SB[i] > delta) {
+        console.log(SA.length - j);
+        counter += SA.length - j;
+        break;
+      }
     }
   }
+  for (var i = SB.length - 1; i >= 0; i--) {
+    var j = SA.length - 1;
+    for (; j >= 0; j--) {
+      if (SB[i] - SA[j] > delta) {
+        console.log(j + 1);
+        counter += j + 1;
+        break;
+      }
+    }
+  }
+  console.log(counter);
 
-  res.send({ result: counter });
+  res.send({ result: SA.length * SB.length - counter });
 });
 
 export default router;
