e218d1fa4f793482f5a5a93c65d6f7bf352df4c6
mcreng <tsehonam@gmail.com>
2018-09-23 04:32:37 +0800

    Optimized more

--- routes/dino.js
+++ routes/dino.js
@@ -11,14 +11,31 @@ function loop() {
     data[[0, k]] = 0;
   }
   data[[0, 0]] = 1;
+
+  var new_nonzero = { 0: true };
   for (var i = 1; i <= n; i++) {
+    var nonzero = new_nonzero,
+      new_nonzero = {};
     for (var k = -amax; k < amax; k++) {
-      var sum =
-        data[[i - 1, k - A[i - 1]]] +
-        data[[i - 1, k + B[i - 1]]] +
-        data[[i - 1, k - A[i - 1] + B[i - 1]]] +
-        data[[i - 1, k]];
-      data[[i, k]] = sum ? sum : 0;
+      if (
+        nonzero[k] ||
+        nonzero[k - A[i - 1]] ||
+        nonzero[k + B[i - 1]] ||
+        nonzero[k - A[i - 1] + B[i - 1]]
+      ) {
+        var sum1 = data[[i - 1, k - A[i - 1]]];
+        var sum2 = data[[i - 1, k + B[i - 1]]];
+        var sum3 = data[[i - 1, k - A[i - 1] + B[i - 1]]];
+        var sum4 = data[[i - 1, k]];
+
+        var sum =
+          (sum1 ? sum1 : 0) +
+          (sum2 ? sum2 : 0) +
+          (sum3 ? sum3 : 0) +
+          (sum4 ? sum4 : 0);
+        data[[i, k]] = sum ? sum : 0;
+        if (data[[i, k]] !== 0) new_nonzero[k] = true;
+      }
     }
     for (var k = -amax; k < amax; k++) {
       delete data[[i - 1, k]];
@@ -36,8 +53,9 @@ router.post("/", function(req, res, next) {
   amax = A.reduce((a, b) => a + b, 0) + B.reduce((a, b) => a + b, 0) + 1;
   loop();
   var counter = 0;
+  console.log(data);
   for (var i = -delta; i <= delta; i++) {
-    counter += data[[n, i]];
+    counter += data[[n, i]] ? data[[n, i]] : 0;
   }
 
   res.send({ result: counter });
