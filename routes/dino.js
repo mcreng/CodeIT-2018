752329e392942ba86366837a40413be8c2bad083
mcreng <tsehonam@gmail.com>
2018-09-23 10:11:01 +0800

    Changed implementation

--- routes/dino.js
+++ routes/dino.js
@@ -7,67 +7,67 @@ var n = 0;
 var A = null;
 var B = null;
 
-// var elapsed_time = function(note) {
-//   var precision = 3; // 3 decimal places
-//   var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
-//   console.log(
-//     process.hrtime(start)[0] +
-//       " s, " +
-//       elapsed.toFixed(precision) +
-//       " ms - " +
-//       note
-//   ); // print message + time
-//   start = process.hrtime(); // reset the timer
-// };
+var elapsed_time = function(note) {
+  var precision = 3; // 3 decimal places
+  var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+  console.log(
+    process.hrtime(start)[0] +
+      " s, " +
+      elapsed.toFixed(precision) +
+      " ms - " +
+      note
+  ); // print message + time
+  start = process.hrtime(); // reset the timer
+};
 
 var start = process.hrtime();
 
-function loop() {
-  data[[0, 0]] = 1;
-
+function new_loop() {
   var new_nonzero = [0];
   for (var i = 1; i <= n; i++) {
+    var prevData = data;
+    data = {};
     var nonzero = new_nonzero,
       new_nonzero = [];
     nonzero = _.uniq(nonzero);
-    nonzero.forEach(kk => {
-      var kl = [kk, kk + A[i - 1], kk - B[i - 1], kk + A[i - 1] - B[i - 1]];
-      kl.forEach(k => {
-        var sum1 = data[[i - 1, k - A[i - 1]]];
-        var sum2 = data[[i - 1, k + B[i - 1]]];
-        var sum3 = data[[i - 1, k - A[i - 1] + B[i - 1]]];
-        var sum4 = data[[i - 1, k]];
+    elapsed_time([i, nonzero.length]);
+
+    for (var kk of nonzero) {
+      for (var k of [
+        kk,
+        kk + A[i - 1],
+        kk - B[i - 1],
+        kk + A[i - 1] - B[i - 1]
+      ]) {
+        var sum1 = prevData[k - A[i - 1]];
+        var sum2 = prevData[k + B[i - 1]];
+        var sum3 = prevData[k - A[i - 1] + B[i - 1]];
+        var sum4 = prevData[k];
 
         var sum =
           (sum1 ? sum1 : 0) +
           (sum2 ? sum2 : 0) +
           (sum3 ? sum3 : 0) +
           (sum4 ? sum4 : 0);
-        data[[i, k]] = sum ? sum : 0;
-        if (data[[i, k]] !== 0) new_nonzero.push(k);
-      });
-    });
-
-    Object.keys(data).forEach(key => {
-      key = JSON.parse("[" + key + "]");
-      if (key[0] == i - 1) delete data[key];
-    });
+        data[k] = sum ? sum : 0;
+        if (data[k] !== 0) new_nonzero.push(k);
+      }
+    }
   }
 }
 
 router.post("/", function(req, res, next) {
-  data = {};
+  data = { 0: 1 };
   console.log(req.body);
   n = req.body["number_of_types_of_food"];
   A = req.body["calories_for_each_type_for_raphael"];
   B = req.body["calories_for_each_type_for_leonardo"];
   var delta = req.body["maximum_difference_for_calories"];
-  loop();
+  new_loop();
   var counter = 0;
 
   Object.keys(data).forEach(k => {
-    k = JSON.parse("[" + k + "]");
-    if (Math.abs(k[1]) <= delta) {
+    if (Math.abs(k) <= delta) {
       counter += data[k];
       counter %= 100000123;
     }
