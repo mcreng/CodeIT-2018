4c218ba07d7dc0ee9bedaefe7f87550a0091d2e2
Kevin Crylz <33240066+kevinCrylz@users.noreply.github.com>
2018-09-23 10:56:24 +0800

    Update tallyExpense.py

--- routes/tallyExpense.py
+++ routes/tallyExpense.py
@@ -1,5 +1,6 @@
 import json
 import sys
+#input = sys.argv[1]
 
 dict_input = json.loads(sys.argv[1])
 
@@ -13,6 +14,7 @@ trans = dict_input['expenses']
 for tran in trans:
     paidBy = tran["paidBy"]
     amount = tran["amount"]
+    
     exclude_persons = [paidBy]
     if "exclude" in tran:
         exclude = tran["exclude"]
@@ -30,22 +32,23 @@ for tran in trans:
 
 out_trans = {"transactions": []}
 
+balance = sorted(balance.items(), key=lambda x: x[1])
+
 neg_ptr = n_total-1
 for i in range(n_total):
     balance[i] = list(balance[i])
     while balance[i][1] < 0:
         balance[neg_ptr] = list(balance[neg_ptr])    
-        tmp_tran = {"from": balance[i][0], "to": balance[neg_ptr][0], "amount": 0}
         if abs(balance[i][1]) >= abs(balance[neg_ptr][1]):
             balance[i][1] += abs(balance[neg_ptr][1])
-            tmp_tran["amount"] = round(abs(balance[neg_ptr][1]),2)
+            tmp_tran = {"from": balance[i][0], "to": balance[neg_ptr][0], "amount": round(abs(balance[neg_ptr][1]),2)}
             balance[neg_ptr][1] = 0
             neg_ptr -= 1
         else:
             balance[neg_ptr][1] -= abs(balance[i][1])
-            tmp_tran["amount"] = round(abs(balance[i][1]),2)
+            tmp_tran = {"from": balance[i][0], "to": balance[neg_ptr][0], "amount": round(abs(balance[i][1]),2)}
             balance[i][1] = 0
-        out_trans["transaction"].append(tmp_tran)
+        out_trans["transactions"].append(tmp_tran)
 
 print(json.dumps(out_trans))
 sys.stdout.flush()
